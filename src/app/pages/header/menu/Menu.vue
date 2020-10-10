46805e86fd87db5eeddd01aef61a2ffda7352846
Richard Leibowitz <richardbbleibowitz@gmail.com>
2020-10-10 11:26:02 +0300

    wip

--- src/app/pages/header/menu/Menu.vue
+++ src/app/pages/header/menu/Menu.vue
@@ -3,7 +3,7 @@
 <template>
 	<div className="vertical-center">
 		<router-link v-if="isHome && loggedIn" to="/about">About</router-link>
-		<router-link v-else-if="isAbout && loggedIn" to="/">Mahahaklice</router-link>
+		<router-link v-else-if="isAbout && loggedIn" to="/">Lorems</router-link>
 		<span v-else></span>
 	</div>
 </template>

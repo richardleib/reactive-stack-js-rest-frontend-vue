aa3fa05c5672563ab88aabc66c4723e138955fb0
Richard Leibowitz <richardbbleibowitz@gmail.com>
2021-05-20 23:18:30 +0300

    refactoring

--- src/app/pages/lorems/preview/preview.js
+++ src/app/pages/lorems/preview/preview.js
@@ -5,7 +5,7 @@ import moment from 'moment';
 import router from '@/router';
 import Versions from './versions/Versions.vue';
 import LocalStore from '@/_reactivestack/store/local.store';
-import {sendGet} from '@/services/_f.send.fetch';
+import {sendGet} from '@/factories/_f.send.fetch';
 
 export default {
 	name: 'Preview',
